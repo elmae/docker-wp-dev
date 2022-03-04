b5e2d7cce0d3847303bceb7bd494316990f0f16f
elmae <dmilesdrsgs@gmail.com>
2022-03-04 10:03:24 -0400

    incluido tema understrap

--- src/themes/understrap/global-templates/navbar-collapse-bootstrap4.php
+++ src/themes/understrap/global-templates/navbar-collapse-bootstrap4.php
@@ -13,41 +13,45 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
-	<h2 id="main-nav-label" class="screen-reader-text">
-		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-	</h2>
+    <h2 id="main-nav-label" class="screen-reader-text">
+        <?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+    </h2>
 
 
-<?php if ( 'container' === $container ) : ?>
-	<div class="container">
-<?php endif; ?>
+    <?php if ( 'container' === $container ) : ?>
+    <div class="container">
+        <?php endif; ?>
 
-		<!-- Your site title as branding in the menu -->
-		<?php if ( ! has_custom_logo() ) { ?>
+        <!-- Your site title as branding in the menu -->
+        <?php if ( ! has_custom_logo() ) { ?>
 
-			<?php if ( is_front_page() && is_home() ) : ?>
+        <?php if ( is_front_page() && is_home() ) : ?>
 
-				<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+        <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<?php else : ?>
+        <?php else : ?>
 
-				<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+        <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+            itemprop="url"><?php bloginfo( 'name' ); ?></a>
 
-			<?php endif; ?>
+        <?php endif; ?>
 
-			<?php
+        <?php
 		} else {
 			the_custom_logo();
 		}
 		?>
-		<!-- end custom logo -->
+        <!-- end custom logo -->
 
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false"
+            aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-		<!-- The WordPress Menu goes here -->
-		<?php
+        <!-- The WordPress Menu goes here -->
+        <?php
 		wp_nav_menu(
 			array(
 				'theme_location'  => 'primary',
@@ -62,8 +66,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 		);
 		?>
 
-<?php if ( 'container' === $container ) : ?>
-	</div><!-- .container -->
-<?php endif; ?>
+        <?php if ( 'container' === $container ) : ?>
+    </div><!-- .container -->
+    <?php endif; ?>
 
 </nav><!-- .site-navigation -->
--- src/themes/understrap/global-templates/navbar-collapse-bootstrap5.php
+++ src/themes/understrap/global-templates/navbar-collapse-bootstrap5.php
@@ -10,42 +10,51 @@ defined( 'ABSPATH' ) || exit;
 
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
+<div class="container d-none d-lg-block">
+    <div class="row">
+        <div class="col-md-4 py-4">Hola</div>
+        <div class="col-md-8 py-4">algo va a ir aqui</div>
+    </div>
+</div>
+<nav id="main-nav" class="navbar sticky-top navbar-expand-md navbar-dark bg-primary">
 
-<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
-
-	<h2 id="main-nav-label" class="screen-reader-text">
-		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-	</h2>
+    <h2 id="main-nav-label" class="screen-reader-text">
+        <?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+    </h2>
 
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+    <div class="<?php echo esc_attr( $container ); ?>">
 
-		<!-- Your site title as branding in the menu -->
-		<?php if ( ! has_custom_logo() ) { ?>
+        <!-- Your site title as branding in the menu -->
+        <?php if ( ! has_custom_logo() ) { ?>
 
-			<?php if ( is_front_page() && is_home() ) : ?>
+        <?php if ( is_front_page() && is_home() ) : ?>
 
-				<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+        <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<?php else : ?>
+        <?php else : ?>
 
-				<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+        <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+            itemprop="url"><?php bloginfo( 'name' ); ?></a>
 
-			<?php endif; ?>
+        <?php endif; ?>
 
-			<?php
+        <?php
 		} else {
 			the_custom_logo();
 		}
 		?>
-		<!-- end custom logo -->
+        <!-- end custom logo -->
 
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false"
+            aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-		<!-- The WordPress Menu goes here -->
-		<?php
+        <!-- The WordPress Menu goes here -->
+        <?php
 		wp_nav_menu(
 			array(
 				'theme_location'  => 'primary',
@@ -60,6 +69,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 		);
 		?>
 
-	</div><!-- .container(-fluid) -->
+    </div><!-- .container(-fluid) -->
 
 </nav><!-- .site-navigation -->
