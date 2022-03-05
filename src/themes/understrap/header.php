804b4df352b61f98eecb1173ba355db041bc3c1c
elmae <dmilesdrsgs@gmail.com>
2022-03-05 15:53:22 -0400

    incluido header top

--- src/themes/understrap/header.php
+++ src/themes/understrap/header.php
@@ -15,22 +15,26 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
+  <?php do_action( 'wp_body_open' ); ?>
+  <div class="site" id="page">
+    <!-- header top -->
+    <?php get_template_part( 'global-templates/header-top'); ?>
+    <!-- end header top -->
+    <!-- ******************* The Navbar Area ******************* -->
+    <header id="wrapper-navbar" class="sticky-top">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<header id="wrapper-navbar">
+      <a class="skip-link sr-only sr-only-focusable"
+        href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+      <?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
 
-		<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
-
-	</header><!-- #wrapper-navbar end -->
+    </header><!-- #wrapper-navbar end -->
