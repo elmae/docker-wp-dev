13734e0429662be7e76509448144a5a5517aa09a
elmae <dmilesdrsgs@gmail.com>
2022-03-05 17:43:43 -0400

    moviendo logo a header top

--- src/themes/understrap/global-templates/header-top.php
+++ src/themes/understrap/global-templates/header-top.php
@@ -7,15 +7,38 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="container d-none d-lg-block">
+<div class="<?php echo esc_attr( $container ); ?> d-none d-lg-block">
   <div class="row">
     <!-- Logo -->
-    <div class="col-md-4 py-4">Hola m</div>
+    <div class="col-md-4 py-4">
+      <!-- Your site title as branding in the menu -->
+      <?php if ( ! has_custom_logo() ) { ?>
+
+      <?php if ( is_front_page() && is_home() ) : ?>
+
+      <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+          itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+      <?php else : ?>
+
+      <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+        itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+      <?php endif; ?>
+
+      <?php
+      } else {
+        the_custom_logo();
+      }
+      ?>
+      <!-- end custom logo -->
+    </div>
     <!-- end logo -->
     <!-- social links -->
-    <div class="col-md-8 py-4">algo va a ir aqui</div>
+    <div class="col-md-8 py-4 d-flex align-items-center justify-content-end">algo va a ir aqui</div>
     <!-- end social links -->
   </div>
 </div>
