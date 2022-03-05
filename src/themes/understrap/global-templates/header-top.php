fec0d0170923f649f5e5f6d32899155f1016df65
elmae <dmilesdrsgs@gmail.com>
2022-03-05 16:31:27 -0400

    todo updated

--- src/themes/understrap/global-templates/header-top.php
+++ src/themes/understrap/global-templates/header-top.php
@@ -7,14 +7,12 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-
-$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="container d-none d-lg-block">
   <div class="row">
     <!-- Logo -->
-    <div class="col-md-4 py-4">Hola</div>
+    <div class="col-md-4 py-4">Hola m</div>
     <!-- end logo -->
     <!-- social links -->
     <div class="col-md-8 py-4">algo va a ir aqui</div>
